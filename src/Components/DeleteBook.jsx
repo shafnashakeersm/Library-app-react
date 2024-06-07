f584cab5a1cd7a2cd27550d43b9ced7164dbe9a9
shafnashakeer <shafnasm20@gmail.com>
2024-06-07 13:39:55 +0530

    completed

--- src/Components/DeleteBook.jsx
+++ src/Components/DeleteBook.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBook from './NavBook'
 
 const DeleteBook = () => {
   return (
     <div>
+        <NavBook/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -12,7 +14,7 @@ const DeleteBook = () => {
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-warning">DELETE</button>
+                            <button className="btn btn-danger">DELETE</button>
                         </div>
                     </div>
                 </div>
