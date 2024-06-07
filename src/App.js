b771ab69c1508b423e50b12b8eee05f47f1b3e34
shafnashakeer <shafnasm20@gmail.com>
2024-06-07 12:57:35 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBook from './Components/AddBook';
 import SearchBook from './Components/SearchBook';
+import DeleteBook from './Components/DeleteBook';
 
 function App() {
   return (
     <div >
       <AddBook />
      <SearchBook/>
+     <DeleteBook/>
       
     </div >
   );
