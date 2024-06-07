ba3e6dcad623fa1be68596fa87fd2e960aedc714
shafnashakeer <shafnasm20@gmail.com>
2024-06-07 12:53:08 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './Components/AddBook';
+import SearchBook from './Components/SearchBook';
 
 function App() {
   return (
     <div >
       <AddBook />
-     
+     <SearchBook/>
       
     </div >
   );
